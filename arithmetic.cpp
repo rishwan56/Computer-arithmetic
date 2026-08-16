71a572f48791df0bb79085820a713adc666aae0b
rishwan56 <wanrish5647@gmail.com>
2026-08-16 14:24:31 +0530

    code update

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,20 +1,20 @@
 #include "arithmetic.h"
 long long mutliply(int multiplicand, int multiplier) {
-long long product = 0;
-int sign;
-if(multiplicand < 0 && multiplier < 0) sign = 1;
-else if (multiplicand < 0 || multiplier < 0) sign = -1;
-else sign = 1;
-multiplicand = multiplicand >= 0 ? multiplicand : -multiplicand; 
-multiplier = multiplier >= 0 ? multiplier : -multiplier;
-    while(multiplier > 0)
-    {
-        if(multiplier & 1)
-            product += multiplicand;
+	long long product = 0;
+	int sign;
+	if(multiplicand < 0 && multiplier < 0) sign = 1;
+	else if (multiplicand < 0 || multiplier < 0) sign = -1;
+	else sign = 1;
+	multiplicand = multiplicand >= 0 ? multiplicand : -multiplicand; 
+	multiplier = multiplier >= 0 ? multiplier : -multiplier;
+    	while(multiplier > 0)
+    	{
+        	if(multiplier & 1)
+            	product += multiplicand;
 
-        multiplicand <<= 1;
-        multiplier >>= 1;
-    }
-return (product * sign);
+        	multiplicand <<= 1;
+        	multiplier >>= 1;
+    	}
+	return (product * sign);
 }
 
--- build/CMakeFiles/arithmetic_app.dir/arithmetic.cpp.o.d
+++ build/CMakeFiles/arithmetic_app.dir/arithmetic.cpp.o.d
@@ -1,3 +1,4 @@
 CMakeFiles/arithmetic_app.dir/arithmetic.cpp.o: \
  /home/rishwan56/rishwan_program/projects/Computer-arithmetic/arithmetic.cpp \
- /usr/include/stdc-predef.h
+ /usr/include/stdc-predef.h \
+ /home/rishwan56/rishwan_program/projects/Computer-arithmetic/arithmetic.h
